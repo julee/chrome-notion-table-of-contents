e347499d208d5ba9ce12c559698d1b963c9165dc
Cside <cside.story@gmail.com>
2022-10-01 22:33:08 +0900

    最低限の骨組み

---
+++ webpack/webpack.common.js
@@ -0,0 +1,33 @@
+const webpack = require("webpack");
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const srcDir = path.join(__dirname, "..", "src");
+
+module.exports = {
+    entry: {
+      background: path.join(srcDir, 'background.ts'),
+      content_script: path.join(srcDir, 'content_script.tsx'),
+    },
+    output: {
+        path: path.join(__dirname, "../dist/js"),
+        filename: "[name].js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: ".", to: "../", context: "public" }],
+            options: {},
+        }),
+    ],
+};
