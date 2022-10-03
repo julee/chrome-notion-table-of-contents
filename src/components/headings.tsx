ae0166345721b91d60aa9356c11a3233db2ccde1
Cside <cside.story@gmail.com>
2022-10-03 18:06:03 +0900

    ToC が書き換わったら再描画

--- src/components/headings.tsx
+++ src/components/headings.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { waitFor } from '../utils';
 
+const pageContentSelector = '.notion-frame .notion-scroller';
+
 export default () => {
   const [headings, setHeadings] = useState<{
     blockId: string;
@@ -13,32 +15,75 @@ export default () => {
     const target = document.querySelector<HTMLElement>(`[data-block-id="${blockId}"]`);
     if (!target) { return; }
 
-    document.querySelector('.notion-frame .notion-scroller')?.scroll({
+    document.querySelector(pageContentSelector)?.scroll({
       top: target.offsetTop,
     });
   };
 
-  useEffect(() => {
-    (async () => {
-      console.log('# fetch heading');
+  let pageContent: HTMLElement;
+  const refreshAllHeadings = async () => {
+    console.log('# fetch heading');
 
-      let newHeadings = [...headings];
-      const pageContent = (await waitFor('.notion-frame .notion-scroller'))[0];
-      for (const heading of pageContent.querySelectorAll('[placeholder="Heading 1"],[placeholder="Heading 2"],[placeholder="Heading 3"]')) {
-        newHeadings.push({
-          text: heading.textContent || '',
-          rank: Number((heading.getAttribute('placeholder') || '').replace(/^Heading /, '')),
-          blockId: (heading.parentNode?.parentNode as Element).getAttribute('data-block-id') || '',
-        });
+    if (!pageContent) {
+      const elem = document.querySelector(pageContentSelector);
+      if (!elem) {
+        throw new Error(`element '${pageContentSelector}' is not found`);
       }
-      if (headings.length !== 0 && Math.min.apply(null, headings.map(h => h.rank)) !== 1) {
-        newHeadings = headings.map(heading => {
-          heading.rank--;
-          return heading;
-        });
+      pageContent = elem as HTMLElement;
+
+    }
+    let newHeadings = [...headings];
+    const elems = pageContent.querySelectorAll(
+      '[placeholder="Heading 1"],' +
+      '[placeholder="Heading 2"],' +
+      '[placeholder="Heading 3"]'
+    );
+    for (const heading of elems) {
+      const parentElem = heading.closest('[data-block-id]');
+      if (!parentElem) {
+        console.error(parentElem);
+        throw new Error('parent element is not found');
       }
-      setHeadings(newHeadings);
+      newHeadings.push({
+        text: heading.textContent || '',
+        rank: Number((heading.getAttribute('placeholder') || '').replace(/^Heading /, '')),
+        blockId: parentElem.getAttribute('data-block-id') || '',
+      });
+    }
+    if (headings.length !== 0 && Math.min.apply(null, headings.map(h => h.rank)) !== 1) {
+      newHeadings = headings.map(heading => {
+        heading.rank--;
+        return heading;
+      });
+    }
+    setHeadings(newHeadings);
+  };
+
+  useEffect(() => {
+    let observer: MutationObserver;
+    (async () => {
+      pageContent = (await waitFor(pageContentSelector))[0];
+      await refreshAllHeadings();
+
+      const toc = document.querySelector('.notion-table_of_contents-block');
+      if (toc === null) { console.debug('ToC is not found. cannot observe.'); }
+      observer = new MutationObserver((mutationList: MutationRecord[]) => {
+        // 部分更新は行わない
+        // - 親の a まで遡って 、URL を blockId 化して ... とやることは多い
+        //   - 該当の blockId を state の配列を 1 つずつ探索するので、計算量もそこまで変わらない
+        //     - 多くてたかだが O(10) である
+        // - 致命的なことに、新規追加ノードの場合は挿入位置を特定できないので、その際はどのみち全探索になる
+        refreshAllHeadings();
+      });
+      observer.observe(toc as Node, {
+        subtree: true,
+        characterData: true,
+      });
     })();
+    return () => {
+      console.log('# unmount && disconnect observer');
+      observer.disconnect();
+    };
   }, []);
 
   return <>
