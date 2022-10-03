7e404d5996480c8c8cb71167596e1e4bd5a9786b
Cside <cside.story@gmail.com>
2022-10-03 20:26:43 +0900

    refactor

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -3,22 +3,20 @@ import { createRoot } from 'react-dom/client';
 import HeadingsContainer from './components/headingsContainer';
 import { waitFor } from './utils';
 
-((async () => {
-  let isMounted = false;
 
-  // browserAction is clicked
-  chrome.runtime.onMessage.addListener(async (arg) => {
-    if (!isMounted) {
-      const root = document.createElement('div');
-      document.body.appendChild(root);
+let isMounted = false;
+// browserAction is clicked
+chrome.runtime.onMessage.addListener(async () => {
+  if (!isMounted) {
+    const root = document.createElement('div');
+    document.body.appendChild(root);
 
-      createRoot(root).render(<HeadingsContainer />);
-      isMounted = true;
-      return;
-    }
-    const eventReceiver = (await waitFor('#toc-event-receiver'))[0];
-    // toggle viisbility
-    const event = new CustomEvent('actionClicked');
-    eventReceiver.dispatchEvent(event);
-  });
-}))();
+    createRoot(root).render(<HeadingsContainer />);
+    isMounted = true;
+    return;
+  }
+  const eventReceiver = (await waitFor('#toc-event-receiver'))[0];
+  // toggle viisbility
+  const event = new CustomEvent('actionClicked');
+  eventReceiver.dispatchEvent(event);
+});
