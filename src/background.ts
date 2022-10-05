f5c0fb5d917c9f2c9cd8c0a3f9aa1ba8f2533b1a
Cside <cside.story@gmail.com>
2022-10-05 22:50:30 +0900

    error handling

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,12 @@
-chrome.action.onClicked.addListener(() => {
-  chrome.tabs.query({active: true, currentWindow: true}, (tabs: chrome.tabs.Tab[]) => {
-    // TODO: error handleing (when content script is not loaded)
-    chrome.tabs.sendMessage(tabs[0]?.id ?? 0, {});
-  });
+chrome.action.onClicked.addListener(async () => {
+  const tabs = await chrome.tabs.query({active: true, currentWindow: true});
+  try {
+    await chrome.tabs.sendMessage(tabs[0]?.id ?? 0, {});
+  } catch (error) {
+    if (error)
+      console.error({
+        message: 'tabs.sendMessage failed. Maybe content script is not loaded yet',
+        error,
+      });
+  }
 });
