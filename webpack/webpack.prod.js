38cb9b3242fc73956990012330da4a6bd3173f37
Cside <cside.story@gmail.com>
2022-11-28 23:20:22 +0900

    production で console.* 消す

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,6 +1,16 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+  plugins: [
+    new TerserPlugin({
+      terserOptions: {
+        compress: {
+          pure_funcs: ['console.log', 'console.info'],
+        },
+      },
+    }),
+  ],
 });
