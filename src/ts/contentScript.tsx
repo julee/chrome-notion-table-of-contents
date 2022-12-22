d5a1835fc4f443585b4f6e169a1095b6f8792e01
Cside <cside.story@gmail.com>
2022-12-22 10:16:02 +0900

    Feature: Notion blog 等で content script を実行しないように

--- src/ts/contentScript.tsx
+++ src/ts/contentScript.tsx
@@ -4,16 +4,22 @@ import '../postcss/style.pcss';
 import Container from './components/Container';
 import { waitFor } from './utils';
 
-const root = document.createElement('div');
-root.className = 'toc-react-root';
-document.body.appendChild(root);
+// Notion blog とかでも content script が読み込まれるのは無駄なので
+// bg.js でホスト名や要素で絞ってもいいが、、
+// content script の on/off には declarativeContent のようなスマートな API もないし
+// 仰々しいコードになってしまいそうなので、この判定方法で妥協
+if (document.getElementById('notion-app')) {
+  const root = document.createElement('div');
+  root.className = 'toc-react-root';
+  document.body.appendChild(root);
 
-const pageList = await waitFor('.notion-sidebar .notion-scroller');
-const sidebar = pageList.parentNode;
-if (!sidebar)
-  throw new Error(
-    "$('.notion-sidebar .notion-scroller').parentNode is not found",
-  );
-sidebar.insertBefore(root, pageList);
+  const pageList = await waitFor('.notion-sidebar .notion-scroller');
+  const sidebar = pageList.parentNode;
+  if (!sidebar)
+    throw new Error(
+      "$('.notion-sidebar .notion-scroller').parentNode is not found",
+    );
+  sidebar.insertBefore(root, pageList);
 
-createRoot(root).render(<Container />);
+  createRoot(root).render(<Container />);
+}
