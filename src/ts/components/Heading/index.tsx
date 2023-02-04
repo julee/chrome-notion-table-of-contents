388dcce63f35b42813300295001870fda34ed5df
Cside <cside.story@gmail.com>
2023-02-04 15:36:22 +0900

    Feature: Add clicked heading with color animation

--- src/ts/components/Heading/index.tsx
+++ src/ts/components/Heading/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { $, getContainer } from '../../utils';
 
+const animationDuration = 1_500;
+
 export default function Heading({ blockId, isFocused, level, text }: Heading) {
   const scrollToHeading = () => {
+    const heading = $(`[data-block-id="${blockId}"]`);
     getContainer().scroll({
-      top: $(`[data-block-id="${blockId}"]`).offsetTop,
+      top: heading.offsetTop,
     });
+    const textElem = heading.querySelector<HTMLElement>(
+      '[contenteditable="true"]',
+    );
+    if (!textElem)
+      throw new Error(
+        `data-block-id="${blockId}" exists, but inner text element doestn't exist.`,
+      );
+    textElem.style.animation = `clicked-heading ${animationDuration / 1_000}s`;
+    setTimeout(() => {
+      textElem.style.animation = '';
+    }, animationDuration);
   };
   return (
     <p
