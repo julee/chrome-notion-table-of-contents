7465368c2edfd81c9016113a592ea644098f1f0a
Cside <cside.story@gmail.com>
2023-03-21 01:25:37 +0900

    goodbye useTheme

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,5 @@
 import { useAtomValue, useSetAtom } from 'jotai';
-import React, {
-  ReactNode,
-  Suspense,
-  useEffect,
-  useLayoutEffect,
-  useState,
-} from 'react';
+import React, { Suspense, useEffect, useLayoutEffect, useState } from 'react';
 import { throttle } from 'throttle-debounce';
 import {
   handlePageMoveAtom,
@@ -20,11 +14,10 @@ import Headings from '../Headings/Headings';
 import TailFoldButton from '../TailFoldButton/TailFoldButton';
 import './common.pcss';
 import './customProperties.css';
-import { ThemeContext, useTheme } from './hooks';
 import './styles.pcss';
 
 const Main = () => {
-  const theme = useTheme();
+  const [theme, setTheme] = useState<Theme>(THEME.LIGHT);
   const wholeFolded = useAtomValue(wholeFoldedAtom);
 
   const handlePageMove = useSetAtom(handlePageMoveAtom);
@@ -34,6 +27,13 @@ const Main = () => {
     const fn = throttle(() => handleResize(), THROTTLE_TIME);
     window.addEventListener('resize', fn);
     return () => window.removeEventListener('resize', fn);
+  }, []);
+
+  useLayoutEffect(() => {
+    (async () => {
+      const elem = await waitFor('.notion-light-theme,.notion-dark-theme');
+      setTheme(elem.matches('.notion-light-theme') ? THEME.LIGHT : THEME.DARK);
+    })();
   }, []);
 
   usePageMoveEvent(() => {
@@ -52,28 +52,10 @@ const Main = () => {
   );
 };
 
-const ThemeContextProvider = ({ children }: { children: ReactNode }) => {
-  const [theme, setTheme] = useState<Theme>(THEME.LIGHT);
-
-  // set theme
-  useLayoutEffect(() => {
-    (async () => {
-      const elem = await waitFor('.notion-light-theme,.notion-dark-theme');
-      setTheme(elem.matches('.notion-light-theme') ? THEME.LIGHT : THEME.DARK);
-    })();
-  }, []);
-
-  return (
-    <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>
-  );
-};
-
 export default function App() {
   return (
-    <ThemeContextProvider>
-      <Suspense fallback={<div></div>}>
-        <Main />
-      </Suspense>
-    </ThemeContextProvider>
+    <Suspense fallback={<div></div>}>
+      <Main />
+    </Suspense>
   );
 }
--- src/components/App/hooks.ts
+++
@@ -1,8 +0,0 @@
-import { createContext, useContext } from 'react';
-
-export const ThemeContext = createContext<Theme | undefined>(undefined);
-export const useTheme = () => {
-  const theme = useContext(ThemeContext);
-  if (!theme) throw new Error('Wrap this component with Provider.');
-  return theme;
-};
